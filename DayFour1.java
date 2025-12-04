4bdd3e1f92fc6c583eb4660c647e9299513090b5
Nick Taylor <42318378+ntaylor3@users.noreply.github.com>
2025-12-04 10:06:45 +0000

    Day Four

---
+++ DayFour1.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.Vector;
+
+public class DayFour1 {
+    public static void main(String[] args) throws IOException {
+
+        // read all lines from input file
+        BufferedReader bf = new BufferedReader(new FileReader("input4.txt"));
+        Vector<String> lines = new Vector<String>();
+        while (bf.ready()) {
+            String line = bf.readLine();
+            lines.add(line);
+        }
+
+        // figure the height and width of the grid
+        int height = lines.size();
+        int width = lines.get(0).length();
+
+        // feed characters into an array
+        char[][] shelves = new char[height][width];
+        for (int i = 0; i < height; i++) {
+            String line = lines.get(i);
+            for (int j = 0; j < width; j++) {
+                shelves[i][j] = line.charAt(j);
+            }
+        }
+
+        int accessible = 0;
+
+        // iterate array and check each direction from the current position
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (shelves[i][j] == '@') {
+                    int blocked = 0;
+                    if (i > 0) {
+
+                        if (j > 0 && shelves[i - 1][j - 1] == '@')
+                            blocked++;
+                        if (shelves[i - 1][j] == '@')
+                            blocked++;
+                        if (j < width - 1 && shelves[i - 1][j + 1] == '@')
+                            blocked++;
+                    }
+
+                    if (j > 0 && shelves[i][j - 1] == '@')
+                        blocked++;
+                    if (j < width - 1 && shelves[i][j + 1] == '@')
+                        blocked++;
+
+                    if (i < height - 1) {
+                        if (j > 0 && shelves[i + 1][j - 1] == '@')
+                            blocked++;
+                        if (shelves[i + 1][j] == '@')
+                            blocked++;
+                        if (j < width - 1 && shelves[i + 1][j + 1] == '@')
+                            blocked++;
+                    }
+
+                    if (blocked < 4)
+                        accessible++;
+                }
+
+                System.out.println(accessible);
+
+            }
+        }
+
+        bf.close();
+    }
+}
