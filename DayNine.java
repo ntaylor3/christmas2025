4bfe6adc38753603a314b9134747ac58021971e0
Nick Taylor <42318378+ntaylor3@users.noreply.github.com>
2025-12-09 13:16:16 +0000

    Tidy up

--- DayNine.java
+++ DayNine.java
@@ -19,33 +19,33 @@ public class DayNine {
             Point point = new Point(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
             points.add(point);
             polygon.addPoint(point.x, point.y);
-        }  
+        }
 
         long biggestArea1 = 0;
         long biggestArea2 = 0;
-        
-        //run through all possible rectangles defined by two nodes
-        for (int i = 0; i < points.size(); i++) {
-            for (int j = i+1; j < points.size(); j++) {
 
-                //work out area of each rectangle
+        // run through all possible rectangles defined by two nodes
+        for (int i = 0; i < points.size(); i++) {
+            for (int j = i + 1; j < points.size(); j++) {
+
+                // work out area of each rectangle
                 int width = Math.abs(points.get(i).x - points.get(j).x) + 1;
                 int height = Math.abs(points.get(i).y - points.get(j).y) + 1;
                 long area = (long) width * (long) height;
 
-                //Part 1: find the biggest one for
+                // Part 1: find the biggest one for
                 if (area > biggestArea1) {
                     biggestArea1 = area;
                 }
 
-                //Part 2: define a rectangle that's actually very slightly smaller than the target
-                //due to how contains() works with edges
+                // Part 2: define a rectangle that's actually very slightly smaller than the
+                // target due to how contains() works with edges
                 int x1 = Math.min(points.get(i).x, points.get(j).x);
                 int y1 = Math.min(points.get(i).y, points.get(j).y);
-                //need to remove the 1 that was added to the width and the height!
-                Rectangle2D.Double rect = new Rectangle2D.Double(x1+0.1, y1 + 0.1, width - 1.2, height - 1.2);
+                // need to remove the 1 that was added to the width and the height!
+                Rectangle2D.Double rect = new Rectangle2D.Double(x1 + 0.1, y1 + 0.1, width - 1.2, height - 1.2);
 
-                //check if rectangle is in the polygon
+                // check if rectangle is in the polygon
                 if (polygon.contains(rect)) {
                     if (area > biggestArea2) {
                         biggestArea2 = area;
