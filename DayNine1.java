9e71e17ec63da10f0ee3cba76261f13254a1ec8e
Nick Taylor <42318378+ntaylor3@users.noreply.github.com>
2025-12-09 12:17:10 +0000

    Tweaks

--- DayNine1.java
+++ DayNine1.java
@@ -19,34 +19,32 @@ public class DayNine1 {
             Point point = new Point(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
             points.add(point);
             polygon.addPoint(point.x, point.y);
-        }  
+        }
 
         long biggestArea1 = 0;
         long biggestArea2 = 0;
-        
-        //run through all possible rectangles defined by two nodes
-        for (int i = 0; i < points.size(); i++) {
-            for (int j = 0; j < points.size(); j++) {
 
-                //work out area of each rectangle
+        // run through all possible rectangles defined by two points
+        for (int i = 0; i < points.size(); i++) {
+            for (int j = i + 1; j < points.size(); j++) {
+
+                // work out area of each rectangle
                 int width = Math.abs(points.get(i).x - points.get(j).x) + 1;
                 int height = Math.abs(points.get(i).y - points.get(j).y) + 1;
                 long area = (long) width * (long) height;
 
-                //Part 1: find the biggest one for
+                // Part 1: find the biggest one
                 if (area > biggestArea1) {
                     biggestArea1 = area;
                 }
 
-                //Part 2: define a rectangle that's actually very slightly smaller than the target
-                //due to how contains() works with edges
-                int x1 = Math.min(points.get(i).x, points.get(j).x);
-                int y1 = Math.min(points.get(i).y, points.get(j).y);
-                int x2 = Math.max(points.get(i).x, points.get(j).x);
-                int y2 = Math.max(points.get(i).y, points.get(j).y);
-                Rectangle2D.Double rect = new Rectangle2D.Double(x1+0.1, y1 + 0.1, x2 - x1 - 0.2, y2 - y1 - 0.2);
+                // Part 2: define a rectangle that's actually very slightly smaller than the
+                // target due to how contains() works with edges
+                int x = Math.min(points.get(i).x, points.get(j).x);
+                int y = Math.min(points.get(i).y, points.get(j).y);
+                Rectangle2D.Double rect = new Rectangle2D.Double(x + 0.1, y + 0.1, width - 0.2, height - 0.2);
 
-                //check if rectangle is in the polygon
+                // check if rectangle is in the polygon
                 if (polygon.contains(rect)) {
                     if (area > biggestArea2) {
                         biggestArea2 = area;
