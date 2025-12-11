815ba6638d69de511731f9eefd6763b9848dbf5b
Nick Taylor <42318378+ntaylor3@users.noreply.github.com>
2025-12-11 16:43:42 +0000

    Clean up

--- DayEleven2.java
+++ DayEleven2.java
@@ -19,22 +19,22 @@ import java.util.List;
 public class DayEleven2 {
     public static void main(String[] args) throws IOException {
 
-        HashMap<String, Server> servers = new HashMap<String, Server>();
+        HashMap<String, Server2> servers = new HashMap<String, Server2>();
 
         // create each server
         List<String> lines = Files.readAllLines(new File("input11.txt").toPath());
         for (String line : lines) {
 
             String[] parts = line.split(" ");
-            Server origin = null;
+            Server2 origin = null;
             // run through all servers and connections on this line
             for (int i = 0; i < parts.length; i++) {
                 parts[i] = parts[i].substring(0, 3);
 
                 // try to pull the server if it already exists, or creatre it
-                Server temp = servers.get(parts[i]);
+                Server2 temp = servers.get(parts[i]);
                 if (temp == null) {
-                    temp = new Server(parts[i]);
+                    temp = new Server2(parts[i]);
                     servers.put(parts[i], temp);
                 }
 
@@ -61,173 +61,54 @@ public class DayEleven2 {
                 { "out" }
         };
 
+        // run through each stage finding connections ot the next
         for (int stage = 0; stage < keyNodes.length - 1; stage++) {
             String[] sources = keyNodes[stage];
             String[] destinations = keyNodes[stage + 1];
 
             for (int i = 0; i < sources.length; i++) {
                 for (int j = 0; j < destinations.length; j++) {
-                    Server source = servers.get(sources[i]);
-                    Server dest = servers.get(destinations[j]);
+                    Server2 source = servers.get(sources[i]);
+                    Server2 dest = servers.get(destinations[j]);
 
+                    // in the first stage just add the new paths
+                    // after that, we need to mutiply them with the previous set
                     long paths = source.getPathsTo(dest, 10);
                     if (stage == 0) {
-                        dest.incomingPaths += paths; // first stage
+                        dest.incomingPaths += paths;
                     } else {
                         dest.incomingPaths += (paths * source.incomingPaths);
                     }
-                    System.out.println(source.name + "-" + dest.name + ": " + paths + " " + dest.incomingPaths);
                 }
             }
         }
 
-        /*
-         * String[] sources = { "svr" };
-         * String[] destinations = { "flm", "cas", "eqq" };
-         * 
-         * for (int i = 0; i < sources.length; i++) {
-         * for (int j = 0; j < destinations.length; j++) {
-         * Server source = servers.get(sources[i]);
-         * Server dest = servers.get(destinations[j]);
-         * 
-         * long paths = source.getPathsTo(dest, 10);
-         * dest.incomingPaths += paths;
-         * System.out.println(source.name + "-" + dest.name + ": " + paths + " " +
-         * dest.incomingPaths);
-         * }
-         * }
-         * 
-         * sources = destinations;
-         * destinations = new String[] { "fft" };
-         * 
-         * for (int i = 0; i < sources.length; i++) {
-         * for (int j = 0; j < destinations.length; j++) {
-         * Server source = servers.get(sources[i]);
-         * Server dest = servers.get(destinations[j]);
-         * 
-         * long paths = source.getPathsTo(dest, 10);
-         * dest.incomingPaths += (paths * source.incomingPaths);
-         * System.out.println(source.name + "-" + dest.name + ": " + paths + " " +
-         * dest.incomingPaths);
-         * }
-         * }
-         * 
-         * sources = destinations;
-         * destinations = new String[] { "fhz", "qzp", "gxv" };
-         * 
-         * for (int i = 0; i < sources.length; i++) {
-         * for (int j = 0; j < destinations.length; j++) {
-         * Server source = servers.get(sources[i]);
-         * Server dest = servers.get(destinations[j]);
-         * 
-         * long paths = source.getPathsTo(dest, 10);
-         * dest.incomingPaths += (paths * source.incomingPaths);
-         * System.out.println(source.name + "-" + dest.name + ": " + paths + " " +
-         * dest.incomingPaths);
-         * }
-         * }
-         * 
-         * sources = destinations;
-         * destinations = new String[] { "xhu", "mdm", "cjm", "jmk", "sdo" };
-         * 
-         * for (int i = 0; i < sources.length; i++) {
-         * for (int j = 0; j < destinations.length; j++) {
-         * Server source = servers.get(sources[i]);
-         * Server dest = servers.get(destinations[j]);
-         * 
-         * long paths = source.getPathsTo(dest, 10);
-         * dest.incomingPaths += (paths * source.incomingPaths);
-         * System.out.println(source.name + "-" + dest.name + ": " + paths + " " +
-         * dest.incomingPaths);
-         * }
-         * }
-         * 
-         * sources = destinations;
-         * destinations = new String[] { "hll", "ovp", "exc", "wze", "ktf" };
-         * 
-         * for (int i = 0; i < sources.length; i++) {
-         * for (int j = 0; j < destinations.length; j++) {
-         * Server source = servers.get(sources[i]);
-         * Server dest = servers.get(destinations[j]);
-         * 
-         * long paths = source.getPathsTo(dest, 10);
-         * dest.incomingPaths += (paths * source.incomingPaths);
-         * System.out.println(source.name + "-" + dest.name + ": " + paths + " " +
-         * dest.incomingPaths);
-         * }
-         * }
-         * 
-         * sources = destinations;
-         * destinations = new String[] { "dac" };
-         * 
-         * for (int i = 0; i < sources.length; i++) {
-         * for (int j = 0; j < destinations.length; j++) {
-         * Server source = servers.get(sources[i]);
-         * Server dest = servers.get(destinations[j]);
-         * 
-         * long paths = source.getPathsTo(dest, 10);
-         * dest.incomingPaths += (paths * source.incomingPaths);
-         * System.out.println(source.name + "-" + dest.name + ": " + paths + " " +
-         * dest.incomingPaths);
-         * }
-         * }
-         * 
-         * sources = destinations;
-         * destinations = new String[] { "ngs", "tml", "you", "daz" };
-         * 
-         * for (int i = 0; i < sources.length; i++) {
-         * for (int j = 0; j < destinations.length; j++) {
-         * Server source = servers.get(sources[i]);
-         * Server dest = servers.get(destinations[j]);
-         * 
-         * long paths = source.getPathsTo(dest, 10);
-         * dest.incomingPaths += (paths * source.incomingPaths);
-         * System.out.println(source.name + "-" + dest.name + ": " + paths + " " +
-         * dest.incomingPaths);
-         * }
-         * }
-         * 
-         * sources = destinations;
-         * destinations = new String[] { "out" };
-         * 
-         * for (int i = 0; i < sources.length; i++) {
-         * for (int j = 0; j < destinations.length; j++) {
-         * Server source = servers.get(sources[i]);
-         * Server dest = servers.get(destinations[j]);
-         * 
-         * long paths = source.getPathsTo(dest, 10);
-         * dest.incomingPaths += (paths * source.incomingPaths);
-         * System.out.println(source.name + "-" + dest.name + ": " + paths + " " +
-         * dest.incomingPaths);
-         * }
-         * }
-         */
-
+        System.out.println("Part 2: " + servers.get("out").incomingPaths);
     }
 }
 
-class Server {
+class Server2 {
     String name;
-    HashSet<Server> outputs = new HashSet<Server>();
+    HashSet<Server2> outputs = new HashSet<Server2>();
 
     // incoming paths is used only by key nodes to track number of paths leading to
     // them
     long incomingPaths = 0;
 
     // server with three letter code
-    public Server(String name) {
+    public Server2(String name) {
         this.name = name;
     }
 
     // add output connection to another server
-    public void addOutput(Server server) {
+    public void addOutput(Server2 server) {
         outputs.add(server);
     }
 
     // get number of paths from here to the target server
     // the limit serves to prevent us going too deep now that the
     // graph is segmented
-    public long getPathsTo(Server target, int limit) {
+    public long getPathsTo(Server2 target, int limit) {
         if (target == this) {
             // if this is the target, path is 1
             return 1;
@@ -237,7 +118,7 @@ class Server {
         } else {
             // otherwise, sum paths from all outputs to the target
             long total = 0;
-            for (Server s : outputs) {
+            for (Server2 s : outputs) {
                 total += s.getPathsTo(target, limit - 1);
             }
             return total;
