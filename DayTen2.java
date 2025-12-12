75731056b0b11570336fbda9ffbbde1dbcddde48
Nick Taylor <42318378+ntaylor3@users.noreply.github.com>
2025-12-12 15:32:56 +0000

    Day 10.2 - A pyrrhic victory

--- DayTen2.java
+++ DayTen2.java
@@ -2,175 +2,145 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
-// This was an attempt to solve part 2 of day 10 using a similar approach to part 1,
-// which works for the test data but runs out of memory on the full input!
+// Bit of a weird solution - I had already built the linear equations in Java but don't
+// know how to solve them here, so I output Python code to use Z3 to solve them.
+// I don't really know Python so this was the best I could do!
+// There is a Java interface to Z3 but I couldn't find good examples of setting up
+// the solver and equations so I went with this horrible approach instead.
 
 public class DayTen2 {
     public static void main(String[] args) throws IOException {
 
-        // create machines from each line of input and solve
+        // create machines from each line of input and construct neccessary Python code
+        String python = "from z3 import * \n\n";
+        python += "runningTotal = 0\n\n";
         List<String> lines = Files.readAllLines(new File("input10.txt").toPath());
-        int total = 0;
         for (String line : lines) {
-            Machine machine = new Machine(line);
-            total += machine.solve();
+            EquationSet machine = new EquationSet(line);
+            python += machine.getPython() + "\n\n";
         }
-        System.out.println("Part 1: " + total);
+
+        // output the python code to a file
+        Files.write(new File("DayTen2.py").toPath(), python.getBytes());
+        System.out.println("Python code written to DayTen2.py");
     }
 }
 
-class Machine {
+// Represents a single equation of the form a + b + c + ... = result
+class Equation {
+    HashSet<Character> variables;
+    int result;
 
-    ArrayList<Button> buttons = new ArrayList<Button>();
-    int[] targetState;
+    // initiate with the result value
+    public Equation(int result) {
+        this.variables = new HashSet<Character>();
+        this.result = result;
+    }
+
+    // add a variable to the equation
+    public void addVariable(char variable) {
+        this.variables.add(variable);
+    }
+
+    // convert the equation to a string representation that will be used in Python
+    // code
+    public String toString() {
+
+        String equation = "";
+        for (Character variable : variables) {
+            equation += variable + " + ";
+        }
+        if (!equation.equals(""))
+            equation = equation.substring(0, equation.length() - 3);
+        return equation + " == " + result;
+    }
+}
+
+// a set of simultaneous equations representing the machine
+class EquationSet {
+
+    ArrayList<Equation> equestions = new ArrayList<Equation>();
+    int numButtons = 0;
 
     // parse input string to create machine
-    public Machine(String target) {
-        String[] parts = target.split(" "); 
+    public EquationSet(String source) {
 
-        System.out.println("Parsing machine: " + target);  
-        
-        int length = 0;
+        // split into parts and trim off the brackets
+        String[] parts = source.split(" ");
+        numButtons = parts.length - 2;
         for (int i = 0; i < parts.length; i++) {
             parts[i] = parts[i].substring(1, parts[i].length() - 1);
-            if (i == 0) {
-                //use the wiring diagram to quickly get the length
-                length = parts[i].length();
-            } else if (i == parts.length - 1) {
-                //parse the joltage
-                System.out.println("Parsing target state: " + parts[i]);
-                String[] joltageParts = parts[i].split(",");
-                targetState = new int[joltageParts.length];
-                for (int j = 0; j < joltageParts.length; j++) {
-                    targetState[j] = Integer.parseInt(joltageParts[j]);
-                }
-                System.out.println("Target State: " + Arrays.toString(targetState));
-            } else {
-                // for the remaining parts, use them to create buttons
-                System.out.println("Cresting button from: " + parts[i]);
+        }
 
-                buttons.add(new Button(parts[i], length));
+        // get the last part which is the target
+        String target = parts[parts.length - 1];
+
+        // break up the target into each joltage dial...
+        String[] targetParts = target.split(",");
+
+        // for each target part, create an equation
+        for (int i = 0; i < targetParts.length; i++) {
+            targetParts[i] = targetParts[i].trim();
+            equestions.add(new Equation(Integer.parseInt(targetParts[i])));
+        }
+
+        // now go back to the original input and look at the other bits...
+        for (int i = 1; i < parts.length - 1; i++) {
+
+            // add this button's variable to each equation it affects
+            String[] subParts = parts[i].split(",");
+            for (String subPart : subParts) {
+                int j = Integer.parseInt(subPart.trim());
+                equestions.get(j).addVariable((char) ('a' + i - 1));
             }
         }
     }
 
-    // solve the machine and return the minimum number of presses
-    public int solve() {
+    // generate Python code to solve the equations using Z3
+    // I'm so sorry
+    public String getPython() {
+        String python = "";
 
-        // list to hold all attempts at the current depth
-        ArrayList<Attempt> currentAttempts = new ArrayList<Attempt>();
-
-        // crate an intial attempt with 0 button presses
-        Attempt attempt = new Attempt(this.targetState, new ArrayList<Button>());
-        int depth = 0;
-        currentAttempts.add(attempt);
-
-        while (true) {
-            // at the new depth, spawn a new layer of attempts from the current attempts
-            // adding an extra button to each one
-            depth++;
-            System.out.println("Depth: " + depth + " Current Attempts: " + currentAttempts.size());
-            ArrayList<Attempt> newAttempts = new ArrayList<Attempt>();
-            for (Attempt currentAttempt : currentAttempts) {
-                if (!currentAttempt.isStuck()) {
-                    for (Button button : this.buttons) {
-                        Attempt child = currentAttempt.getChild(button);
-                        if (child.isComplete()) {
-                            // if the child is compelte, return the depth and finish
-                            return depth;
-                        } else if (!child.isStuck()) {
-                            // if the child is not stuck, add it to the new attempts for the next layer
-                            // otherwise it can be abandoned
-                            newAttempts.add(child);
-                        }
-                    }
-                }
-            }
-            currentAttempts = newAttempts;
+        // create variables and solver
+        for (int i = 0; i < numButtons; i++) {
+            python += (char) ('a' + i) + " = Int('" + (char) ('a' + i) + "')\n";
         }
-    }
-}
+        python += "x = Int('x')\n";
+        python += "s = Solver()\n";
 
-class Button {
-
-    int[] mask;
-
-    public Button(String input, int numLights) {
-
-        //build the bit mask for this button, i.e if it toggles a light
-        //then that bit is 1, otherwise 0
-        mask = new int[numLights];
-        String[] parts = input.split(",");
-        for (int i = 0; i < numLights; i++) {
-            for (int j = 0; j < parts.length; j++) {
-                if (Integer.parseInt(parts[j]) == i) {
-                    mask[i] = 1;
-                }
-            }
-        }
-    }
-}
-
-class Attempt {
-
-    //HashSet<int[]> seenStates = new HashSet<int[]>();
-    boolean stuck = false;
-    boolean complete = false;
-    Button nextButton;
-    ArrayList<Button> buttonSequence;
-    int[] targetState;
-
-    // an attempt at solving the puzzle with a sequence of button presses
-    // and a target state
-    public Attempt(int[] targetState, ArrayList<Button> buttonSequence) {
-
-        this.targetState = targetState;
-        this.buttonSequence = buttonSequence;
-
-        int[] currentState = new int[targetState.length];
-
-        // run the sequence of button presses
-        for (Button b : buttonSequence) {
-            //System.out.println("Current State before: " + Arrays.toString(currentState));
-            //System.out.println("Applying mask: " + Arrays.toString(b.mask));
-            for (int i = 0; i < currentState.length; i++) {
-                currentState[i] = currentState[i] + b.mask[i];
-            }
-             //System.out.println("Current State after: " + Arrays.toString(currentState));
+        // add equations to solver
+        for (Equation eq : equestions) {
+            python += "s.add(" + eq.toString().replace(" = ", " == ") + ")\n";
         }
 
-        //check if anything has overshot and mark us as stuck
-        for (int i = 0; i < currentState.length; i++) {
-            if (currentState[i] > targetState[i]) {
-                stuck = true;
-                //System.out.println("Overshoot!");
-            }
+        // add the x variable to represent total presses
+        python += "s.add(x == ";
+        for (int i = 0; i < numButtons; i++) {
+            python += (char) ('a' + i) + " + ";
         }
+        python = python.substring(0, python.length() - 3) + ")\n";
 
-        //if it mathches the target state, we're complete
-        if (Arrays.equals(currentState, targetState)) {
-            complete = true;
+        // add constraints that all variables are non-negative
+        python += "s.add(x >= 0,";
+        for (int i = 0; i < numButtons; i++) {
+            python += (char) ('a' + i) + " >= 0,";
         }
+        python = python.substring(0, python.length() - 1) + ")\n";
+
+        // final chunk of code finds the minimum x value satisfying the equations
+        python += "tempTotal = 0\n";
+        python += "while s.check() == sat:\n";
+        python += "    if tempTotal==0 or s.model()[x].as_long() < tempTotal:\n";
+        python += "        tempTotal = s.model()[x].as_long()\n";
+        python += "    s.add(x < s.model()[x])\n";
+        python += "print(tempTotal)\n";
+        python += "runningTotal += tempTotal\n";
+        python += "print(runningTotal)\n";
+        python += "print(\"Done\")\n";
+
+        return python;
     }
-
-    public Attempt getChild(Button button) {
-        // create a new attempt by adding a button press to the sequence
-        ArrayList<Button> newButtonsTried = new ArrayList<Button>(buttonSequence);
-        newButtonsTried.add(button);
-        return new Attempt(targetState, newButtonsTried);
-    }
-
-    public boolean isStuck() {
-
-        return stuck;
-    }
-
-    public boolean isComplete() {
-
-        return complete;
-    }
-
 }
